e5a6da99842424ec4b1e79dab5047b8f403c53af
William Wang <rongxing.wang@hotmail.com>
2017-11-21 16:18:07 -0800

    Update Verify-DBALatestBackupFiles.ps1

--- Verify-DBALatestBackupFiles.ps1
+++ Verify-DBALatestBackupFiles.ps1
@@ -1,5 +1,5 @@
 param($src_server,$src_port,$src_database)
-
+#Verify the existenance of backup files
 $backup_files = Get-DBALatestBackupFiles $src_server $src_port $src_database
 
 Foreach ($b in $backup_files) {
