93a0a7830dd001fec6f246c82e4a67cf1fc8945c
William Wang <rongxing.wang@hotmail.com>
2018-05-09 18:17:07 -0700

    Update test.ps1

--- OGG/test.ps1
+++ OGG/test.ps1
@@ -1,53 +1,73 @@
 $src_server = 'S1'
 $src_mgr_port = 7809
-$ogg_home = 'C:\OGG'
-$dsn = 'ogg_dsn'
 $tgt_server = 'S2'
 $tgt_mgr_port = 7809
+$src_ogg_home = 'C:\OGG'
+$tgt_ogg_home = 'C:\OGG'
+$src_dsn = 'ogg_dsn'
+$tgt_dsn = 'ogg_dsn'
+
 
 ############################
 ##Execute GGSCI commands
 ##Invoke-Command -Session $s -FilePath Invoke-GGSCI.ps1 -ArgumentList $ogg_home, $ggsci_command
 ############################
 $ggsci_command = @"
-DBLOGIN SOURCEDB $dsn
+DBLOGIN SOURCEDB $src_dsn
 ADD TRANDATA dbo.*
 "@
 
 Set-Location $PSScriptRoot
 $s = New-PSSession -ComputerName $src_server
-Invoke-Command -Session $s -FilePath Invoke-GGSCI.ps1 -ArgumentList $ogg_home, $ggsci_command
+Invoke-Command -Session $s -FilePath Invoke-GGSCI.ps1 -ArgumentList $src_ogg_home, $ggsci_command
 
 ############################
 ##Create/Edit Param file
 ##Invoke-Command -Session $s -FilePath Edit-OggPram.ps1 -ArgumentList $ogg_home, $proc_name, $statement
 ##(TBD) Add -append switch
 ############################
-$type = 'EXTRACT'
-$proc_name = 'cdcext'
-$trail_name = 'ce'
+$extract_name = 'cdcext'
+$local_trail_name = 'ce'
+
 $statement = @"
-$type $proc_name
-SOURCEDB $dsn
-EXTTRAIL ./dirdat/$trail_name
+EXTRACT $extract_name
+SOURCEDB $src_dsn
+EXTTRAIL ./dirdat/$local_trail_name
 
 TABLE dbo.*;
 "@
 
-Invoke-Command -Session $s -FilePath Edit-OggPram.ps1 -ArgumentList $ogg_home, $proc_name, $statement
+Invoke-Command -Session $s -FilePath Edit-OggPram.ps1 -ArgumentList $src_ogg_home, $extract_name, $statement
 
-$type = 'EXTRACT'
-$proc_name = 'cdcpmp'
-$trail_name = 'cp'
+#Add Extract
+$ggsci_command = @"
+ADD EXTRACT $extract_name, TRANLOG, BEGIN NOW
+ADD EXTTRAIL ./dirdat/$local_trail_name, EXTRACT $extract_name
+"@
+
+Invoke-Command -Session $s -FilePath Invoke-GGSCI.ps1 -ArgumentList $src_ogg_home, $ggsci_command
+
+#Add Pump
+$pump_name = 'cdcpmp'
+$remote_trail_name = 'cp'
+
 $statement = @"
-$type $proc_name
+EXTRACT $pump_name
 RMTHOST $tgt_server MGRPORT $tgt_mgr_port
-RMTTRAIL ./dirdat/$trail_name
+RMTTRAIL ./dirdat/$remote_trail_name
 
 TABLE dbo.*;
 "@
 
-Invoke-Command -Session $s -FilePath Edit-OggPram.ps1 -ArgumentList $ogg_home, $proc_name, $statement
+Invoke-Command -Session $s -FilePath Edit-OggPram.ps1 -ArgumentList $src_ogg_home, $pump_name, $statement
+
+
+$ggsci_command = @"
+ADD EXTRACT $pump_name, EXTTRAILSOURCE ./dirdat/$local_trail_name
+ADD RMTTRAIL ./dirdat/$remote_trail_name, EXTRACT $pump_name
+"@
+
+Invoke-Command -Session $s -FilePath Invoke-GGSCI.ps1 -ArgumentList $src_ogg_home, $ggsci_command
 
 
 
