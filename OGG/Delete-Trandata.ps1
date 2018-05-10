391105dfe2cbd790dd68f969042b8403c8035d3d
William Wang <rongxing.wang@hotmail.com>
2018-05-10 00:21:40 -0700

    Create Delete-Trandata.ps1

---
+++ OGG/Delete-Trandata.ps1
@@ -0,0 +1,5 @@
+############################
+##DELETE TRANDATA
+##Example: 
+##Invoke-Command -Session $s -FilePath Delete-Trandata.ps1 -ArgumentList $ogg_dsn, $ogg_home, $schema_name, $table_name
+############################
