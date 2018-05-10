be4749558a9f6bdbfab7516fecab7707ec5d676d
William Wang <rongxing.wang@hotmail.com>
2018-05-10 00:21:11 -0700

    Create Add-Trandata.ps1

---
+++ OGG/Add-Trandata.ps1
@@ -0,0 +1,5 @@
+############################
+##ADD TRANDATA
+##Example: 
+##Invoke-Command -Session $s -FilePath Add-Trandata.ps1 -ArgumentList $ogg_dsn, $ogg_home, $schema_name, $table_name
+############################
