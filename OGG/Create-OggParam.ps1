f23719f2c500fa23a787350eced3a0a36283ad1d
William Wang <rongxing.wang@hotmail.com>
2018-05-10 21:50:34 -0700

    Update Create-OggParam.ps1

--- OGG/Create-OggParam.ps1
+++ OGG/Create-OggParam.ps1
@@ -1,8 +1,7 @@
 ############################
-##Create/Edit Param file
+##Create Param file
 ##Example:
-##Invoke-Command -Session $s -FilePath Edit-OggPram.ps1 -ArgumentList $src_ogg_home, $pump_name, $statementInvoke-Command -Session $s -FilePath Edit-OggPram.ps1 -ArgumentList $ogg_home, $proc_name, $statement
-##(TBD) Add -append switch
+##Invoke-Command -Session $s -FilePath Create-OggParam.ps1 -ArgumentList $src_ogg_home, $extract_name, $statement
 ############################
 
 
@@ -11,7 +10,6 @@ Param
     [string] $ogg_home,
     [string] $proc_name,
     [string] $statement
-    #[switch] $append
 )
 
 $param_filename = $proc_name + '.prm'
